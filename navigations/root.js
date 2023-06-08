0d95fdcdff2d6dcb91e58fe903e6670e9a7dd322
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-06-08 08:28:51 +0300

    create user navigation file

---
+++ navigations/root.js
@@ -0,0 +1,30 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React, { createContext, useEffect, useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import routes from "../common/routes";
+import axios from "axios";
+import Contact from "../screens/contacts";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import Users from "../screens/users";
+import Todo from "../screens/todo";
+import UsersNavigator from "./usersNavigation";
+export const usersContext = createContext();
+const Root = () => {
+  const Tab = createMaterialTopTabNavigator();
+
+  return (
+    <Tab.Navigator style={styles.container}>
+      <Tab.Screen name={routes.contacts} component={Contact} />
+      <Tab.Screen name="users" component={UsersNavigator} />
+      <Tab.Screen name={routes.todo} component={Todo} />
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Platform.OS === "android" ? 50 : 0,
+  },
+});
+
+export default Root;
